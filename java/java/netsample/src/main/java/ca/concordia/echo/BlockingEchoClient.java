ef6af939f67870b1ba2f738a814c118aadf2a36f
cedparadis <84161247+cedparadis@users.noreply.github.com>
2022-10-12 13:40:57 -0400

    Add files via upload

---
+++ java/java/netsample/src/main/java/ca/concordia/echo/BlockingEchoClient.java
@@ -0,0 +1,102 @@
+package ca.concordia.echo;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class BlockingEchoClient {
+
+    // readFully reads until the request is fulfilled or the socket is closed
+    private static void readFully(SocketChannel socket, ByteBuffer buf, int size) throws IOException {
+        while (buf.position() < size) {
+            int n = socket.read(buf);
+            if (n == -1) {
+                break;
+            }
+        }
+        if (buf.position() != size) {
+            throw new EOFException();
+        }
+    }
+
+    private static void readEchoAndRepeat(SocketChannel socket) throws IOException {
+        Charset utf8 = StandardCharsets.UTF_8;
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            ByteBuffer buf = utf8.encode(line);
+            int n = socket.write(buf);
+            buf.clear();
+
+            // Receive all what we have sent
+            readFully(socket, buf, n);
+            buf.flip();
+            System.out.println("Replied: " + utf8.decode(buf));
+        }
+    	
+    }
+
+    private static void runClient(SocketAddress endpoint) throws IOException {
+        try (SocketChannel socket = SocketChannel.open()) {
+            socket.connect(endpoint);
+            System.out.println("Type any thing then ENTER. Press Ctrl+C to terminate");
+            //readEchoAndRepeat(socket);
+            Socket socket1 = new Socket("httpbin.org", 80);
+            PrintStream out = new PrintStream( socket1.getOutputStream() );
+            BufferedReader in = new BufferedReader( new InputStreamReader( socket1.getInputStream() ) );
+            out.println( "GET /status/418 HTTP/1.0" );
+            out.println();
+            String line = in.readLine();
+            while( line != null )
+            {
+                System.out.println( line );
+                line = in.readLine();
+            }
+         // Close our streams
+            in.close();
+            out.close();
+            socket.close();
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }	
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        OptionParser parser = new OptionParser();
+        parser.acceptsAll(asList("host", "h"), "EchoServer hostname")
+                .withOptionalArg()
+                .defaultsTo("httpbin.org");
+
+        parser.acceptsAll(asList("port", "p"), "EchoServer listening port")
+                .withOptionalArg()
+                .defaultsTo("80");
+
+        OptionSet opts = parser.parse(args);
+
+        String host = (String) opts.valueOf("host");
+        System.out.println(host);
+        int port = Integer.parseInt((String) opts.valueOf("port"));
+
+        SocketAddress endpoint = new InetSocketAddress(host, port);
+        runClient(endpoint);
+    }
+}
+
