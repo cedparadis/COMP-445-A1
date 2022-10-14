4968d6a09c0cb962eb49a8ea8ad9cbdd08e46616
cedparadis <84161247+cedparadis@users.noreply.github.com>
2022-10-14 14:36:28 -0400

    get method completely done

--- java/java/netsample/src/main/java/ca/concordia/echo/BlockingEchoClient.java
+++ java/java/netsample/src/main/java/ca/concordia/echo/BlockingEchoClient.java
@@ -1,94 +1,105 @@
 package ca.concordia.echo;
 
-import static java.util.Arrays.asList;
-
 import java.io.BufferedReader;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
+import java.net.URL;
+import java.util.Map;
 
 public class BlockingEchoClient {
 
-    // readFully reads until the request is fulfilled or the socket is closed
-    private static void readFully(SocketChannel socket, ByteBuffer buf, int size) throws IOException {
-        while (buf.position() < size) {
-            int n = socket.read(buf);
-            if (n == -1) {
-                break;
-            }
-        }
-        if (buf.position() != size) {
-            throw new EOFException();
-        }
-    }
-
-    private static void readEchoAndRepeat(SocketChannel socket) throws IOException {
-        Charset utf8 = StandardCharsets.UTF_8;
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            ByteBuffer buf = utf8.encode(line);
-            int n = socket.write(buf);
-            buf.clear();
-
-            // Receive all what we have sent
-            readFully(socket, buf, n);
-            buf.flip();
-            System.out.println("Replied: " + utf8.decode(buf));
-        }
-    	
-    }
-    
+    private final static String USER_AGENT = "Mozilla/5.0";
+    private final static String ACCEPT_LANG = "en-US,en;q=0.5";
     
     
     /*
      * Send a get request method
      * -open connection
      * -set socket with socket address
-     * -
+     * -read header
+     * -read data
+     * -close connection
+     * 
      */
 
     
-    private static void runClient(SocketAddress endpoint) throws IOException {
-        try (SocketChannel socket = SocketChannel.open()) {
-            socket.connect(endpoint);
-            System.out.println("Type any thing then ENTER. Press Ctrl+C to terminate");
-            //readEchoAndRepeat(socket);
-            Socket socket1 = new Socket("httpbin.org", 80);
+    private static void sendGET(String testUrl, boolean verbose, Map<String,String>headers) throws IOException {
+    		
+    		//put in URL to parse elements
+    		URL url = new URL(testUrl);
+    		String host = url.getHost();
+    		String path = url.getPath();
+    		String param = url.getQuery();
+    		int port = url.getPort() != -1 ? url.getPort():url.getDefaultPort();
+    		//System.out.println("host: " + host + "\nPath: " + param + "\nPort: " + port);
+    		
+    		//open socket to connect to server 
+    		//initialize output buffer to send request
+    		//initialize input buffer to receive response
+            Socket socket1 = new Socket(host, port);
             PrintStream out = new PrintStream( socket1.getOutputStream() );
             BufferedReader in = new BufferedReader( new InputStreamReader( socket1.getInputStream() ) );
-            out.println( "GET /get?assignment=1 HTTP/1.0" );
+            
+            //send the get request with parameters
+            if(path!= null) {
+            	out.println( "GET "+ path + "?"+ param + " HTTP/1.0" );
+            }
+            //without parameters
+            else {
+            	out.println( "GET "+ path + " HTTP/1.0" );
+            }
+            if(headers == null) {
+            	out.println("User-Agent: " + USER_AGENT);
+            	out.println("Accept-language: " + ACCEPT_LANG);
+            	out.println("Host: " + host);
+            }
+            else {
+            	for(String key: headers.keySet()) {
+            		out.println(key +": " + headers.get(key));
+            	}
+            }
             out.println();
             out.println();
             String line = in.readLine();
+            
+            //do not show response header
+            if(!verbose) 
+            {
             while( line != null )
             {
-                System.out.println( line );
-                line = in.readLine();
+            	//condition to not show the response header we skip until we reach two empty lines
+            	if(line.isEmpty() ) {
+            		while(line!= null) {
+            			if(line.isEmpty()) {
+            				line = in.readLine();
+            				continue;
+            			}
+            			 System.out.println( line );
+                         line = in.readLine();
+            		}
+            	}
+            	line = in.readLine();	
+               
+           } 
+            }
+            
+            //show response header
+            else if(verbose){
+            	while(line != null) {
+            		System.out.println( line );
+                    line = in.readLine();
+            	}
             }
          // Close our streams
             in.close();
             out.close();
-            socket.close();
-        }
-        catch( Exception e )
-        {
-            e.printStackTrace();
-        }	
+            socket1.close();
+       }
         
-    }
+     
+    
 
     public static void main(String[] args) throws IOException {
        /* OptionParser parser = new OptionParser();
@@ -106,8 +117,8 @@ public class BlockingEchoClient {
         System.out.println(host);
         int port = Integer.parseInt((String) opts.valueOf("port"));
 */
-        SocketAddress endpoint = new InetSocketAddress("httpbin.org", 80);
-        runClient(endpoint);
+    	Map<String,String>headers = null;
+        sendGET("http://httpbin.org/get?course=networking&assignment=1", false, headers);
     }
 }
 
